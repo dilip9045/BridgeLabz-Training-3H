36f5a5bdeca46d3a1c456eb946172f5f2be75290
Dilip Kumar Sisodiya <Desdeepak42@gmail.com>
2025-09-09 17:26:13 +0530

    Basic_Oops/level1/MobilePhone/MobilePhoneMain.java code

---
+++ Basic_Oops/level1/MobilePhone/MobilePhoneMain.java
@@ -0,0 +1,9 @@
+
+package MobilePhone;
+public class MobilePhoneMain {
+    public static void main(String[] args) {
+        MobilePhone phone = new MobilePhone("Apple", "iPhone 13", 128);
+        phone.displayDetails();
+        
+    }
+}
