0c486dad5aff27b2c2eb49c2f13e8a018ca72776
Dilip Kumar Sisodiya <Desdeepak42@gmail.com>
2025-09-09 17:34:48 +0530

    Basic_Oops/level2/BankAccount/BankAccount.java code

---
+++ Basic_Oops/level2/BankAccount/BankAccount.java
@@ -0,0 +1,29 @@
+package BankAccount;
+public class BankAccount {
+    String accountHolder;
+    String accountNumber;
+    double balance;
+    BankAccount(String accountHolder, String accountNumber, double balance) {
+        this.accountHolder = accountHolder;
+        this.accountNumber = accountNumber;
+        this.balance = balance;
+    }
+    void deposit(double amount) {
+        balance += amount;
+        System.out.println("Deposited: " + amount);
+    }
+    void withdraw(double amount) {
+        if (amount <= balance) {
+            balance -= amount;
+            System.out.println("Withdrawn: " + amount);
+        } else {
+            System.out.println("Insufficient balance!");
+        }
+    }
+    void displayBalance() {
+        System.out.println("Account Holder: " + accountHolder);
+        System.out.println("Account Number: " + accountNumber);
+        System.out.println("Current Balance: " + balance);
+        
+    }
+}
