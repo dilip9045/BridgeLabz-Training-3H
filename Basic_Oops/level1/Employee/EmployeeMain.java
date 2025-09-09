7f6c9a4772b1d911dfb77acd7be2cb7a1cf7995e
Dilip Kumar Sisodiya <Desdeepak42@gmail.com>
2025-09-09 17:21:13 +0530

    Basic_Oops/level1/Employee/EmployeeMain.java code

---
+++ Basic_Oops/level1/Employee/EmployeeMain.java
@@ -0,0 +1,8 @@
+package Employee;
+public class EmployeeMain {
+    public static void main(String[] args) {
+        Employee emp1 = new Employee("rajesh", 101, 50000);
+        emp1.displayDetails();
+        
+    }
+}
