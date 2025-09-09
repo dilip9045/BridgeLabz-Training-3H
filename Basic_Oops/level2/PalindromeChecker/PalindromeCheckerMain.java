777e2d6159b846e3a9e236b4d09cc1ee2bf0c779
Dilip Kumar Sisodiya <Desdeepak42@gmail.com>
2025-09-09 17:39:49 +0530

    Basic_Oops/level2/PalindromeChecker/PalindromeCheckerMain.java code

---
+++ Basic_Oops/level2/PalindromeChecker/PalindromeCheckerMain.java
@@ -0,0 +1,10 @@
+package PalindromeChecker;
+public class PalindromeCheckerMain {
+    public static void main(String[] args) {
+        PalindromeChecker p1 = new PalindromeChecker("madam");
+        p1.displayResult();
+        PalindromeChecker p2 = new PalindromeChecker("Hello");
+        p2.displayResult();
+        
+    }
+}
