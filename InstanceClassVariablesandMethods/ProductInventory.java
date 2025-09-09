2125f4cadd7715f84380270dacd281fd1139dbf1
Dilip Kumar Sisodiya <Desdeepak42@gmail.com>
2025-09-09 16:23:24 +0530

    Added ProductInventory.java inside InstanceClassVariablesandMethods

---
+++ InstanceClassVariablesandMethods/ProductInventory.java
@@ -0,0 +1,38 @@
+import java.util.*;
+
+class Product {
+    String productName;
+    double price;
+    static int totalProducts = 0;
+
+    Product(String productName, double price) {
+        this.productName = productName;
+        this.price = price;
+        totalProducts++;
+    }
+
+    void displayProductDetails() {
+        System.out.println("Product Name: " + productName);
+        System.out.println("Price: " + price);
+    }
+
+    static void displayTotalProducts() {
+        System.out.println("Total Products: " + totalProducts);
+    }
+}
+
+public class ProductInventory {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        sc.nextLine();
+        for (int i = 0; i < n; i++) {
+            String name = sc.nextLine();
+            double price = sc.nextDouble();
+            sc.nextLine();
+            Product p = new Product(name, price);
+            p.displayProductDetails();
+        }
+        Product.displayTotalProducts();
+    }
+}
