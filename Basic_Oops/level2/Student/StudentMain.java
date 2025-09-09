0946c7a8448e371e1d62322de5c60abed38132a3
Dilip Kumar Sisodiya <Desdeepak42@gmail.com>
2025-09-09 17:43:33 +0530

    Basic_Oops/level2/Student/StudentMain.java code

---
+++ Basic_Oops/level2/Student/StudentMain.java
@@ -0,0 +1,10 @@
+
+package Student;
+public class StudentMain {
+    public static void main(String[] args) {
+        Student s1 = new Student("yash", 101, 92.5);
+        s1.calculateGrade();
+        s1.displayDetails();
+        
+    }
+}
