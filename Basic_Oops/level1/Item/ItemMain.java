5c7136b8d36065e9706c35e384e42a7d82e1daa2
Dilip Kumar Sisodiya <Desdeepak42@gmail.com>
2025-09-09 17:23:28 +0530

    Basic_Oops/level1/Item/ItemMain.java

---
+++ Basic_Oops/level1/Item/ItemMain.java
@@ -0,0 +1,8 @@
+package Item;
+public class ItemMain {
+    public static void main(String[] args) {
+        Item i1=new Item(101,"Pen",10.0);   
+        i1.displayItemDetails();
+        
+    }
+}
